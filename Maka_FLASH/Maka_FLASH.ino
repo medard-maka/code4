bcf430c26212f367e9d9882bff78d6f05cde8d96
medard-maka <145594531+medard-maka@users.noreply.github.com>
2024-06-27 18:23:08 +0100

    Update Maka_FLASH.ino
    Signed-off-by: medard-maka <145594531+medard-maka@users.noreply.github.com>

--- Maka_FLASH/Maka_FLASH.ino
+++ Maka_FLASH/Maka_FLASH.ino
@@ -1,7 +1,10 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <EEPROM.h>
-
+#include <SoftwareSerial.h>    
+SoftwareSerial GSM(8, 9); 
+char phone_no[]="+243995799692"; 
+char inchar; 
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // Adresse I2C de l'écran LCD, 16 colonnes et 2 lignes
 int led1=3;
 int led2=4;
@@ -24,21 +27,86 @@ void setup() {
   lcd.setCursor(0, 1);
   lcd.print("Debit:   ");
 
+
+Serial.println("Initializing....");
+initModule("AT","OK",1000);                //Once the handshake test is successful, it will back to OK
+initModule("ATE1","OK",1000);              //this command is used for enabling echo
+initModule("AT+CPIN?","READY",1000);       //this command is used to check whether SIM card is inserted in GSM Module or not
+initModule("AT+CMGF=1","OK",1000);         //Configuring TEXT mode
+initModule("AT+CNMI=2,2,0,0,0","OK",1000); //Decides how newly arrived SMS messages should be handled  
+Serial.println("Initialized Successfully"); 
+
+
   attachInterrupt(digitalPinToInterrupt(waterFlowPin), pulseCounter, FALLING);
   pinMode(led1,OUTPUT);
   pinMode(led2,OUTPUT);
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {
+if(GSM.available() >0){inchar=GSM.read(); Serial.print(inchar); 
+ if(inchar=='R'){inchar=GSM.read(); 
+ if(inchar=='I'){inchar=GSM.read();
+ if(inchar=='N'){inchar=GSM.read();
+ if(inchar=='G'){initModule("ATH","OK",1000); // this command is used for Call busy
+    }
+   }
+  }
+ }
+
+else if(inchar=='P'){delay(10); inchar=GSM.read(); 
+     if(inchar=='R'){delay(10); inchar=GSM.read();
+     if(inchar=='O'){delay(10); inchar=GSM.read();
+
+     if(inchar=='1'){
+              totalVolume=totalVolume+10;
+              sendSMS(phone_no,"j'ai recu 10 litres ");
+              }
+              else if(inchar=='2'){
+              totalVolume=totalVolume+20;
+              sendSMS(phone_no,"j'ai recu 20 litres ");
+              }
+              else if(inchar=='3'){
+              totalVolume=totalVolume+30;
+              sendSMS(phone_no,"j'ai recu 30 litres ");
+      }
+     }
+    }
+  }
+}
+delay(5);
+unsigned long currentMillis = millis();
+ if (currentMillis - previousMillis >= interval) {
     updateFlowRate();
     updateVolume();
-
     updateLCD(totalVolume, flowRate);
-
     previousMillis = currentMillis;
+  }
+}
+
+void sendSMS(char *number, char *msg){
+GSM.print("AT+CMGS=\"");GSM.print(number);GSM.println("\"\r\n"); //AT+CMGS="Mobile Number" <ENTER> - Assigning recipient's mobile number
+delay(500);
+GSM.println(msg); // Message contents
+delay(500);
+GSM.write(byte(26));
+delay(500);  
+}
+
+void initModule(String cmd, char *res, int t){
+while(1){
+    Serial.println(cmd);
+    GSM.println(cmd);
+    delay(100);
+    while(GSM.available()>0){
+       if(GSM.find(res)){
+        Serial.println(res);
+        delay(t);
+        return;
+       }else{Serial.println("Error");
+       }
+       }
+    delay(t);
   }
 }
 
@@ -48,24 +116,14 @@ void pulseCounter() {
 
 void updateFlowRate() {
   detachInterrupt(digitalPinToInterrupt(waterFlowPin));  // Détache l'interruption pendant le calcul du débit
-
   float pulses = pulseCount;
   flowRate = (pulses / interval) * flowFactor;  // Calcule le débit instantané en L/min
-
   pulseCount = 0;  // Réinitialise le compteur d'impulsions
-
   attachInterrupt(digitalPinToInterrupt(waterFlowPin), pulseCounter, FALLING);  // Réattache l'interruption
 }
 
 void updateVolume() {
-  totalVolume -= flowRate * (interval / 60000.0);  // Décrémente le volume écoulé en litres
-
-  // Vous pouvez également ajouter ici une logique pour gérer les erreurs de débit ou filtrer les impulsions indésirables
-if (Serial.available()){
-  int donnee=Serial.read();
-  
-}
-
+  totalVolume -= flowRate * (interval / 60000.0); 
   digitalWrite(led2, LOW);  // Éteint la LED rouge
   digitalWrite(led1, LOW);  // Éteint la LED jaune
 
@@ -76,6 +134,7 @@ if (Serial.available()){
   }
   } 
 }
+
 void updateLCD(float totalVolume, float flow) {
   lcd.setCursor(8, 0);
   lcd.print("       ");  // Efface le champ du volume précédent
